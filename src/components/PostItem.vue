c69ca11f3fbefc3367b712fec14d232d40154de0
briansomogyi <brian.somogyi@emanuel.ro>
2025-04-24 00:24:38 +0300

    Implemented Props and Emits

---
+++ src/components/PostItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div @click="emitClick">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.description }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps(["post"]);
+const emit = defineEmits(["click"]);
+const emitClick = () => emit("click", post.id);
+</script>
